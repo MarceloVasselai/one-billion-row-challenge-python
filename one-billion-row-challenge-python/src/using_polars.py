f33f584b70fe5dd929a4bf0cf6950ce7904a2dbe
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-19 20:59:16 -0300

    alteração Polars

--- one-billion-row-challenge-python/src/using_polars.py
+++ one-billion-row-challenge-python/src/using_polars.py
@@ -4,19 +4,28 @@ import polars as pl
 # Github: https://github.com/koenvo
 # Twitter/x Handle: https://twitter.com/mr_le_fox
 # https://x.com/mr_le_fox/status/1741893400947839362?s=20
+
 def create_polars_df():
-    pl.Config.set_streaming_chunk_size(4000000)
+    # Configura o tamanho do chunk para streaming
+    pl.Config.set_streaming_chunk_size(100000)
+    
+    # Lê o arquivo CSV, aplica transformações e coleta os resultados
     return (
-        
-        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
-        .group_by(by="station")
-        .agg(
-            max = pl.col("measure").max(),
-            min = pl.col("measure").min(),
-            mean = pl.col("measure").mean()
+        pl.scan_csv(
+            "data/measurements.txt",  # Caminho do arquivo
+            separator=";",            # Separador de colunas
+            has_header=False,         # O arquivo não tem cabeçalho
+            new_columns=["station", "measure"],  # Nomes das colunas
+            schema={"station": pl.String, "measure": pl.Float64}  # Esquema das colunas
         )
-        .sort("station")
-        .collect(streaming=True)
+        .group_by(by="station")  # Agrupa por estação
+        .agg(
+            max=pl.col("measure").max(),  # Calcula o valor máximo
+            min=pl.col("measure").min(),  # Calcula o valor mínimo
+            mean=pl.col("measure").mean() # Calcula a média
+        )
+        .sort("station")  # Ordena por estação
+        .collect()  # Coleta os resultados (streaming é automático)
     )
 
 if __name__ == "__main__":
