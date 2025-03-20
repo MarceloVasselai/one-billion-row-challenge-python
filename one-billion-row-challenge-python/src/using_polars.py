919cc1c85188b90cc5dbdf2eea5d914507743689
Marcelo Vasselai <mvasselai@gmail.com>
2025-03-20 10:52:32 -0300

    atualizar todas as versões de opções leitura de dados.

--- one-billion-row-challenge-python/src/using_polars.py
+++ one-billion-row-challenge-python/src/using_polars.py
@@ -1,31 +1,23 @@
 import polars as pl
 
-# Created by Koen Vossen , 
+# Created by Koen Vossen, 
 # Github: https://github.com/koenvo
 # Twitter/x Handle: https://twitter.com/mr_le_fox
 # https://x.com/mr_le_fox/status/1741893400947839362?s=20
-
 def create_polars_df():
-    # Configura o tamanho do chunk para streaming
-    pl.Config.set_streaming_chunk_size(100000)
-    
-    # Lê o arquivo CSV, aplica transformações e coleta os resultados
+    pl.Config.set_streaming_chunk_size(4000000)
     return (
-        pl.scan_csv(
-            "data/measurements.txt",  # Caminho do arquivo
-            separator=";",            # Separador de colunas
-            has_header=False,         # O arquivo não tem cabeçalho
-            new_columns=["station", "measure"],  # Nomes das colunas
-            schema={"station": pl.String, "measure": pl.Float64}  # Esquema das colunas
-        )
-        .group_by(by="station")  # Agrupa por estação
+        
+        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
+        .group_by(by="station")
         .agg(
-            max=pl.col("measure").max(),  # Calcula o valor máximo
-            min=pl.col("measure").min(),  # Calcula o valor mínimo
-            mean=pl.col("measure").mean() # Calcula a média
+            max = pl.col("measure").max(),
+            min = pl.col("measure").min(),
+            mean = pl.col("measure").mean(),
+            station = pl.col("measure").is_last_distinct()
         )
-        .sort("station")  # Ordena por estação
-        .collect()  # Coleta os resultados (streaming é automático)
+        .sort("station")
+        .collect()
     )
 
 if __name__ == "__main__":
